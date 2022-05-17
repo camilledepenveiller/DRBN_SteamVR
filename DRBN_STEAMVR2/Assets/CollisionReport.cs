be5a5df0dbd93c5dae4548887da359e48b807e67
wong <wong.hua@gmail.com>
2022-05-17 12:16:58 +0200

    Solved some bugs, made new ones.

--- DRBN_STEAMVR2/Assets/CollisionReport.cs
+++ DRBN_STEAMVR2/Assets/CollisionReport.cs
@@ -6,42 +6,40 @@ public class CollisionReport : MonoBehaviour
 {
     private Langevin_v2 Lange;
     private SaveContacts SaveContacts;
+    
 
     private void Start()
     {
         Lange = GameObject.Find("Simulation").GetComponent<Langevin_v2>();
+        Debug.Log(Lange == null);
+        Debug.Log("GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH!!!!!!!!!!!");
         SaveContacts = GameObject.Find("Simulation").GetComponent<SaveContacts>();
+        SaveContacts.MatSize =new int[0, 0];
+        Debug.Log(SaveContacts.MatSize.Length + " length!!!!!!!!!!!!!!!!!");
     }
 
     
     private void OnCollisionEnter(Collision collision)
     {
         //discriminate between intra and inter molecular contacts
-        if (SaveContacts.MatSize == null)
-        {
-            Debug.Log("no contacts found");
-        }
-        else if (SaveContacts.MatSize!=null)
-        {
-            Rigidbody Collider = collision.collider.attachedRigidbody;
-            Rigidbody Collother = this.gameObject.GetComponent<Rigidbody>();
 
+        Rigidbody Collider = collision.collider.attachedRigidbody;
+        Rigidbody Collother = this.gameObject.GetComponent<Rigidbody>();
+
+        if (Collider.transform.root == Collother.transform.root)
+        {
+            Debug.Log(Collider.name + Collother.name);
             
-
-            if (Collider.transform.root == Collother.transform.root)
+            if(!Lange.GOS.Contains(Collider) || !Lange.GOS.Contains(Collother))
             {
                 //Debug.Log(Collider.transform.root.name + " " + Collother.transform.root.name);
-                
+
                 int IndexOwn = Lange.GOS.IndexOf(Collider);
                 int IndexOther = Lange.GOS.IndexOf(Collother);
-                //Debug.Log("My Index is " + IndexOwn + " : Its Index is " + IndexOther);
-                //Debug.Log("Collider " + Collider + " : Collother " + Collother);
+                Debug.Log("My Index is " + IndexOwn + " : Its Index is " + IndexOther);
+                Debug.Log("Collider " + Collider + " : Collother " + Collother);
                 SaveContacts.modMatrix(IndexOwn, IndexOther);
             }
-        }
-        else
-        {
-            //Debug.Log("MatSize is null");
         }
     }
 
